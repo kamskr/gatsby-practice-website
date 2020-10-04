1610b20e2712de8a3cd1dd4c2db709945e34863f
Kamil Sikora <sikorakamil26@gmail.com>
2020-10-04 14:16:48 +0200

    styled front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Button from "../components/Button/Button"
 import { graphql } from "gatsby"
+import { ContentWrapper, ImageWrapper } from "../styles/mainPageStyles"
 
 export const query = graphql`
   {
@@ -11,12 +12,15 @@ export const query = graphql`
 `
 const IndexPage = ({ data }) => (
   <>
-    <h1>Hello Hatta</h1>
-    <p>
-      While artists work from real to the abstract, architects must work from
-      the abstract to the real.
-    </p>
-    <img src={data.file.publicURL} />
+    <ContentWrapper>
+      <h1>Your new space</h1>
+      <p>
+        While artists work from real to the abstract, architects must work from
+        the abstract to the real.
+      </p>
+      <Button>estimate project</Button>
+    </ContentWrapper>
+    <ImageWrapper src={data.file.publicURL} />
   </>
 )
 
---
+++ src/styles/mainPageStyles/index.js
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+
+export const ContentWrapper = styled.div`
+  width: 65%;
+  height: calc(100vh - 80px);
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+
+  h1 {
+    font-size: 85px;
+    margin: 0;
+    width: 40%;
+    line-height: 0.9;
+  }
+  p {
+    margin: 20px 0 40px;
+    width: 40%;
+  }
+`
+
+export const ImageWrapper = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 35%;
+  height: 100vh;
+  object-fit: cover;
+`
