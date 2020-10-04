1c8b35cb773cbd46a182d17718ff21834d69ffba
Kamil Sikora <sikorakamil26@gmail.com>
2020-10-04 16:43:47 +0200

    added pages in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,48 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/layouts/post.js`)
+
+  const data = await graphql(
+    `
+      query queryArticles {
+        allMdx {
+          nodes {
+            frontmatter {
+              title
+              slug
+              author
+              featuredImage {
+                childImageSharp {
+                  fluid(maxWidth: 700, maxHeight: 400) {
+                    src
+                  }
+                }
+              }
+            }
+            excerpt(pruneLengths: 50)
+          }
+        }
+      }
+    `
+  )
+
+  result.data.allMdx.nodes.forEach(post => {
+    createPage({
+      // Path for this page — required
+      path: `${post.node.frontmatter.slug}`,
+      component: blogPostTemplate,
+      context: {
+        // Add optional context data to be inserted
+        // as props into the page component..
+        //
+        // The context data can also be used as
+        // arguments to the page GraphQL query.
+        //
+        // The page "path" is always available as a GraphQL
+        // argument.
+      },
+    })
+  })
+}
