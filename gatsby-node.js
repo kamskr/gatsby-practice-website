65766c0676d1e5adf470310e17333e321b48eeb7
Krzysztof Krysiak <krzysztof.krysiak@allbright.io>
2020-11-12 18:58:09 +0100

    add algolia

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,35 @@
 const path = require(`path`)
 const slugify = require("slugify")
 
-exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
-    const blogPostTemplate = path.resolve(`src/layouts/post.js`)
-    const result = await graphql(`
-    query queryCMSPage {
-      allDatoCmsArticle {
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/layouts/post.js`)
+  const result = await graphql(`
+    query {
+      allMdx {
         nodes {
           id
-          title
+          
         }
       }
     }
   `)
+  if (result.errors) {
+    reporter.panic('failed to create posts ', result.errors);
+  }
+  const pages = result.data.allMdx.nodes;
 
-    result.data.allDatoCmsArticle.nodes.forEach(post => {
-        const slugifiedTitle = slugify(post.title, {
-            lower: true,
-        })
-
-        createPage({
-            path: `articles/${slugifiedTitle}`,
-            component: blogPostTemplate,
-            context: {
-                id: post.id,
-            },
-        })
+  pages.forEach(post => {
+    const slugifiedTitle = slugify(post.id, {
+      lower: true,
     })
+
+    createPage({
+      path: `articles/${slugifiedTitle}`,
+      component: blogPostTemplate,
+      context: {
+        id: post.id,
+      },
+    })
+  })
 }
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,55 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
-import { graphql } from 'gatsby';
-import slugify from 'slugify';
-import ArticlePreview from '../components/ArticlePreview/ArticlePreview';
-import PageInfo from '../components/PageInfo/PageInfo';
+import React from "react"
+import styled from "styled-components"
+import { graphql } from "gatsby"
+import slugify from "slugify"
+import ArticlePreview from "../components/ArticlePreview/ArticlePreview"
+import PageInfo from "../components/PageInfo/PageInfo"
 
 const ArticlesWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 50px;
-`;
+`
 
 const pageData = {
-  title: 'articles',
+  title: "articles",
   paragraph: `While artists work from real to the abstract, architects must work from the abstract to the real.`,
-};
+}
 
-const ArticlesPage = () => {
-  // const {
-  //   allDatoCmsArticle: { nodes },
-  // } = data;
+const ArticlesPage = ({ data }) => {
+  const {
+    allMdx: { nodes },
+  } = data
   return (
     <>
-      {/* <PageInfo title={pageData.title} paragraph={pageData.paragraph} />
+      <PageInfo title={pageData.title} paragraph={pageData.paragraph} />
       <ArticlesWrapper>
-        {nodes.map(({ title, featuredImage }) => (
+        {/* {nodes.map(({ title, featuredImage }) => (
           <ArticlePreview
             key={title}
             title={title}
-            image={featuredImage.fluid}
+            image={featuredImage.childImageSharp.fluid}
             slug={slugify(title, { lower: true })}
           />
-        ))}
-      </ArticlesWrapper> */}
+        ))} */}
+      </ArticlesWrapper>
     </>
-  );
-};
+  )
+}
 
-// export const query = graphql`
-//   {
-//     allDatoCmsArticle {
-//       nodes {
-//         title
-//         featuredImage {
-//           fluid(maxWidth: 500) {
-//             ...GatsbyDatoCmsFluid_tracedSVG
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+export const query = graphql`
+  {
+    allMdx {
+      nodes {
+        frontmatter {
+          title
+          featuredImage {
+            childImageSharp {
+              fluid(maxWidth: 500, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
-export default ArticlesPage;
+export default ArticlesPage
